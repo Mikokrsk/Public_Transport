898e999930f06d452a331bbfde74411ad1df414a
Mikokrsk <mikokrsk@gmail.com>
2022-11-11 17:26:40 +0200

    finally

--- Public_Transport/Form1.Designer.cs
+++ Public_Transport/Form1.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(709, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Знайти всі маршрути";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 106);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1920, 452);
+            this.listBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(312, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Станція";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1354, 577);
+            this.ClientSize = new System.Drawing.Size(1924, 577);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Public_Transport/Form1.cs
+++ Public_Transport/Form1.cs
@@ -6,15 +6,75 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Public_Transport
 {
     public partial class Form1 : Form
     {
+        Random gen = new Random();
         public Form1()
         {
             InitializeComponent();
+            update_list_Stops();
+
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            find_routs();
+        }
+
+
+
+        public void find_routs()
+        {
+            
+            CursovaEntities db = new CursovaEntities();
+            listBox1.Items.Clear();
+            listBox1.Items.Add($"Номер маршруту \t Назва маршруту \t\t\t Тип транспорту   \t Водій \t\t\t Початкова зупинка  \tПроміжна  \t\t Проміжна \t\t Проміжна   \t\t Кінцева \t\t Прибуде через");
+            foreach (var item in db.Traffic_schedule)
+            { 
+                if(item.Routes.Stops.Stops_name.Stops_Name1==comboBox1.Text || item.Routes.Stops.Stops_name1.Stops_Name1 == comboBox1.Text||
+                    item.Routes.Stops.Stops_name2.Stops_Name1 == comboBox1.Text || item.Routes.Stops.Stops_name3.Stops_Name1 == comboBox1.Text||
+                    item.Routes.Stops.Stops_name4.Stops_Name1 == comboBox1.Text )
+                {
+                    listBox1.Items.Add($" \t {item.Routes.ID} \t {item.Routes.Name_of_the_Route} \t {item.Routes.Transports.Type_of_Transport} \t {item.Routes.Drivers.Name} \t {item.Routes.Stops.Stops_name.Stops_Name1}" +
+                        $" \t {item.Routes.Stops.Stops_name1.Stops_Name1} \t {item.Routes.Stops.Stops_name2.Stops_Name1} \t {item.Routes.Stops.Stops_name3.Stops_Name1}" +
+                        $" \t {item.Routes.Stops.Stops_name4.Stops_Name1} \t {gen.Next(30)} хв");
+                }
+
+            }
+
+
+        }
+
+        public void  update_list()
+        {
+            CursovaEntities db = new CursovaEntities();
+            listBox1.Items.Clear();
+
+            foreach (var item in db.Traffic_schedule)
+            {
+                listBox1.Items.Add($"{item.ID} \t  {item.Departure_time} \t {item.Arrival_time}  \t {item.Routes.Name_of_the_Route}  \t\t {item.Routes.Drivers.Name}");
+            }
+        }
+        public void update_list_Stops()
+        {
+            CursovaEntities db = new CursovaEntities();
+            comboBox1.Items.Clear();
+            //comboBox2.Items.Clear();
+            foreach (var item in db.Stops_name)
+            {
+
+          //      comboBox2.Items.Add($"{item.Stops_Name1}");
+                comboBox1.Items.Add($"{item.Stops_Name1}");
+
+            }
+        }
+
+
     }
 }
